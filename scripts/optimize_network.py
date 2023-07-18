e6b9ca2d0dc762f29e5e82351332e5bf2c3a7608
lumbric <lumbric@gmail.com>
2023-07-18 16:24:07 +0200

    Add scripts to run things on the VSC

--- scripts/optimize_network.py
+++ scripts/optimize_network.py
@@ -1,3 +1,5 @@
+import os
+
 import subprocess
 from multiprocessing import Pool
 
@@ -13,15 +15,30 @@ def run_subprocess(params):
 
 
 def run_chunk_processes(x_range, y_range):
+    """ """
     pool = Pool(processes=NUM_PROCESSES)
 
-    for x in x_range:
-        for y in y_range:
-            pool.apply_async(run_subprocess, ((x, y),))
+    for x_start_idx in x_range:
+        for y_start_idx in y_range:
+            pool.apply_async(run_subprocess, ((x_start_idx, y_start_idx),))
 
     pool.close()
     pool.join()
 
 
+def run_jobs_slurm(x_range, y_range):
+    for x_start_idx in x_range:
+        for y_start_idx in y_range:
+            command = [
+                "python",
+                "scripts/optimize_network_chunk.py",
+                str(x_start_idx),
+                str(y_start_idx),
+            ]
+            subprocess.run(command, check=True)
+
+
 if __name__ == "__main__":
-    run_chunk_processes(*get_chunk_indices())
+    # this is just a heuristic if we should run via slurm or just multiple processes
+    runner = run_jobs_slurm if 'VSC_CLUSTER_ID' in os.environ else run_chunk_processes
+    runner(*get_chunk_indices())
