87e38cc1de06b0e6225ef00936f40dd0c780f482
lumbric <lumbric@gmail.com>
2023-07-25 16:24:39 +0200

    Run multiple processes on one VSC node

--- scripts/optimize_network.py
+++ scripts/optimize_network.py
@@ -1,44 +1,75 @@
-import os
+"""Create the network for each pixel and run the optimization either in parallel subprocesses or
+as SLURM jobs on the VSC."""
 
+import argparse
 import subprocess
+
 from multiprocessing import Pool
-
 from src.util import get_chunk_indices
-
 from src.config import NUM_PROCESSES
+from src.logging_config import setup_logging
 
 
-def run_subprocess(params):
+def worker(params):
     x_start_idx, y_start_idx = params
     command = ["python", "scripts/optimize_network_chunk.py", str(x_start_idx), str(y_start_idx)]
     subprocess.run(command, check=True)
 
 
-def run_chunk_processes(x_range, y_range):
-    """ """
+def run_chunk_processes(chunks):
+    """Run optimization for chunks in parallel subprocesses."""
     pool = Pool(processes=NUM_PROCESSES)
 
-    for x_start_idx in x_range:
-        for y_start_idx in y_range:
-            pool.apply_async(run_subprocess, ((x_start_idx, y_start_idx),))
+    for x_start_idx, y_start_idx in chunks:
+        pool.apply_async(worker, ((x_start_idx, y_start_idx),))
 
     pool.close()
     pool.join()
 
 
-def run_jobs_slurm(x_range, y_range):
-    for x_start_idx in x_range:
-        for y_start_idx in y_range:
-            command = [
-                "sbatch",
-                "scripts/optimize_network.slrm",
-                str(x_start_idx),
-                str(y_start_idx),
-            ]
-            subprocess.run(command, check=True)
+def run_jobs_slurm(chunks):
+    """Schedule SLURM jobs, each running parallel sub processes."""
+    for i in range(0, len(chunks), NUM_PROCESSES):
+        command = [
+            "sbatch",
+            "scripts/optimize_network.slrm",
+            "--chunks" + "  ".join(f"{x},{y}" for (x, y) in chunks[i : i + NUM_PROCESSES]),
+        ]
+        subprocess.run(command, check=True)
+
+
+def comma_splitter(xy_string):
+    x, y = xy_string.split(",")
+    return int(x), int(y)
+
+
+def main():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        "--vsc",
+        action="store_true",
+        help="VSC mode will schedule jobs via SLURM instead of starting workers as sub processes.",
+    )
+    parser.add_argument(
+        "--chunks",
+        nargs="*",
+        type=comma_splitter,
+        help="x/y start indices for chunks to be optimized in the form 'x1,y1 x2,y2 ...'. If not "
+        "given, all chunks from config will be optimized.",
+    )
+    args = parser.parse_args()
+
+    setup_logging()
+
+    if args.chunks:
+        chunks = args.chunks
+    else:
+        x_range, y_range = get_chunk_indices()
+        chunks = [(x_start_idx, y_start_idx) for x_start_idx in x_range for y_start_idx in y_range]
+
+    runner = run_jobs_slurm if args.vsc else run_chunk_processes
+    runner(chunks)
 
 
 if __name__ == "__main__":
-    # this is just a heuristic if we should run via slurm or just multiple processes
-    runner = run_jobs_slurm if 'VSC_CLUSTER_ID' in os.environ else run_chunk_processes
-    runner(*get_chunk_indices())
+    main()
