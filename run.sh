e41ca896e1d0657174dd77f91a4bb1e62b160bbf
lumbric <lumbric@gmail.com>
2024-02-07 17:53:30 +0100

    Cleanup run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
+# The purpose of this script is to set environment variables and to start Snakemake. Depending on
+# the hostname, a Snakemake file will be guessed.
+#
+# Usage:
+#
+#  ./run.sh
+#
+# or
+#
+#  ./run.sh some_custom=snakemake_parameter
+#
+# -----------------------------------------------------------------------------------------------
+
+
+# This absolute path won't work on the VSC (see README.md), but Gurobi does not work on VSC anyway,
+# so it does not really matter..
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/gurobi1001/linux64/lib
-export GRB_LICENSE_FILE=/home/pregner/gurobi.lic
+export GRB_LICENSE_FILE=$HOME/gurobi.lic
+
 
 # Workaround for CPLEX: the library search path needs to be adjusted for CPLEX to avoid the
 # following error:
@@ -16,16 +33,17 @@ export GRB_LICENSE_FILE=/home/pregner/gurobi.lic
 BASEDIR=$(dirname "$0")
 export PYTHONPATH=$PYTHONPATH:$BASEDIR
 
-# this assumes that the syfop library is simple cloned as subdir
+# This assumes that the syfop library is simple cloned as subdir
 export PYTHONPATH=$PYTHONPATH:$BASEDIR/syfop
 
+
 if [ $(hostname) == "nora" ]; then
-    echo nora
+    echo "Running on nora..."
     export SNAKEMAKE_PROFILE=config/nora
 elif [ -v VSC_CLUSTER_ID ]; then
+    echo "Running via SLURM on VSC..."
     export SNAKEMAKE_PROFILE=config/vsc
 fi
 
 
 snakemake $@
-
