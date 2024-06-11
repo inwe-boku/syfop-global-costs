9f547c3111abf9bc492c6b06ae6c36be6faa3c3b
lumbric <lumbric@gmail.com>
2024-06-11 12:11:04 +0200

    Add pint units

--- src/methanol_network.py
+++ src/methanol_network.py
@@ -1,12 +1,9 @@
+from syfop.units import ureg
 from syfop.util import const_time_series
 from syfop.node import Node, NodeScalableInput, NodeFixOutput, Storage
 from syfop.network import Network
 
 from src.config import SOLVER_DIR
-
-
-# note: for now we don't distinguish between KW and KWh (per time stamp), this is okayish since we
-# have hourly time stamps
 
 
 def create_methanol_network(
@@ -25,22 +22,23 @@ def create_methanol_network(
     methanol_synthesis_input_proportions,
 ):
 
-    #
-    methanol_demand_timeseries = const_time_series(0.0, time_coords=pv_input_profile.time)
-    # FIXME what is the unit here?
+    # We have a time series where the whole demand is during the last time stamp and a free
+    # methanol storage. This is equivalent to a yearly production goal where we don't care when the
+    # methanol is produced.
+    methanol_demand_timeseries = (
+        const_time_series(0.0, time_coords=pv_input_profile.time) * ureg.t / ureg.h
+    )
     methanol_demand_timeseries[-1] = methanol_demand
 
     solar_pv = NodeScalableInput(
         name="solar_pv",
         input_profile=pv_input_profile,
         costs=pv_cost,
-        output_unit="KW",
     )
     wind = NodeScalableInput(
         name="wind",
         input_profile=wind_input_profile,
         costs=wind_cost,
-        output_unit="KW",
     )
     electricity = Node(
         name="electricity",
@@ -48,14 +46,13 @@ def create_methanol_network(
         input_commodities="electricity",
         costs=0,
         storage=Storage(**storage_params["electricity"]),
-        output_unit="KW",
     )
     curtail_electricity = Node(
         name="curtail_electricity",
         inputs=[electricity],
         input_commodities="electricity",
         costs=0,
-        output_unit="KW",
+        size_commodity="electricity",
     )
     electrolizer = Node(
         name="electrolizer",
@@ -63,7 +60,6 @@ def create_methanol_network(
         input_commodities="electricity",
         costs=1 / electrolizer_convert_factor * electrolizer_cost,
         convert_factor=electrolizer_convert_factor,
-        output_unit="t",
         storage=Storage(**storage_params["hydrogen"]),
     )
     co2 = Node(
@@ -72,7 +68,6 @@ def create_methanol_network(
         input_commodities="electricity",
         costs=co2_cost,
         convert_factor=co2_convert_factor,
-        output_unit="t",
         storage=Storage(**storage_params["co2"]),
     )
 
@@ -81,14 +76,15 @@ def create_methanol_network(
         inputs=[co2, electrolizer],
         input_commodities=["co2", "hydrogen"],
         costs=methanol_synthesis_cost,
-        convert_factor=methanol_synthesis_convert_factor,
-        output_unit="KW",
+        convert_factors={
+            "methanol": ("hydrogen", methanol_synthesis_convert_factor),
+        },
         input_proportions=methanol_synthesis_input_proportions,
         # we just set a minimum methanol production per year but do not care
         # when is generated, this is modelled via a free storage:
         storage=Storage(
             **{
-                "costs": 0.0,
+                "costs": 0.0 * ureg.EUR / ureg.t,  # XXX should this be per kWh?
                 "max_charging_speed": 1,
                 "storage_loss": 0,
                 "charging_loss": 0,
@@ -101,9 +97,11 @@ def create_methanol_network(
         inputs=[methanol_synthesis],
         input_commodities=["methanol"],
         output_flow=methanol_demand_timeseries,
-        costs=0,  # XXX why does this cost something if it is fixed and not scalable?
-        output_unit="KW",
     )
+
+    units = {
+        "methanol": ureg.t / ureg.h,
+    }
 
     network = Network(
         [
@@ -116,6 +114,7 @@ def create_methanol_network(
             methanol_synthesis,
             methanol_demand,
         ],
+        units=units,
         time_coords=pv_input_profile.time,
         solver_dir=SOLVER_DIR,
     )
--- src/model_parameters.py
+++ src/model_parameters.py
@@ -1,22 +1,33 @@
+import numpy as np
+from syfop.units import ureg
 from src.wind_costs import wind_costs, OPEX_R
+
+# Values are mostly taken from here:
+# "Pathway to a land-neutral expansion of Brazilian renewable fuel production"
+# https://zenodo.org/records/6471331
+
+
+# XXX costs are per year - we implicitly assume that the model runs for one year
+
 
 # We assume 8% discount rate and 20y life time.
 # n = 20; i = 0.08; ((1+i)**n * i) / ((1+i)**n-1)
 capital_recovery_factor = 0.10185221
 
-battery_cost = 33  # EUR/KWh/a
-hydrogen_storage_cost = 74  # EUR/kg/a
-co2_storage_cost = 0.049  # EUR/kg/a
+battery_cost = 33 * ureg.EUR / ureg.kWh  # EUR/KWh/a
+hydrogen_storage_cost = 74 * ureg.EUR / ureg.kg  # EUR/kg/a
+co2_storage_cost = 0.049 * ureg.EUR / ureg.kg  # EUR/kg/a
 
-electrolizer_cost = 30  # kW/a
-pv_cost = 53  # EUR/kW/a
+electrolizer_cost = 30 * ureg.EUR / ureg.kW  # EUR/kW/a
+pv_cost = 53 * ureg.EUR / ureg.kW  # EUR/kW/a
 # wind_cost = 128  # EUR/kW/a
 
 # XXX wind class 1 is pretty arbitrary, 2020 too
 # height=80m is taken from atlite, since we use Vestas v90 at the moment
-wind_cost = capital_recovery_factor * wind_costs(1, 80, 2020) + OPEX_R[2020]
+wind_cost = capital_recovery_factor * wind_costs(1, 80, 2020) + OPEX_R[2020]  # EUR/kW/a
+wind_cost = wind_cost * ureg.EUR / ureg.kW
 
-electrolizer_efficiency_multiplier = 1
+# electrolizer_efficiency_multiplier = 1
 # electrolizer_efficiency = 0.63 * electrolizer_efficiency_multiplier
 # 0.63 is the value for energy -> energy, if h2 is measured in tons, we use 0.019 according to Jo:
 #     der wert von 0.019 entspricht einer effizienz von 0.63
@@ -25,25 +36,27 @@ electrolizer_efficiency_multiplier = 1
 #
 # 1kg_h2 = 33kWh, zur Herstellung: 1kg_h2 = 33/0.63kWh = 1/0.019 kWh
 # --> 1 kWh = 0.019kg_h2
-electrolizer_convert_factor = 0.019 * 1e-3  # 1KWh --> 1 ton h2
+electrolizer_convert_factor = 0.019 * ureg.kg / ureg.kWh
 
-# -- below here probably constant for all scenarios
 
-methanol_synthesis_cost = 42.0  # in EUR/KW/a - taken from result csv
+# # # below here probably constant for all scenarios without sensitivity analysis # # #
+
+# taken from result csv from brazil paper
+# XXX this might be wrong! 42 EUR/KW/a not EUR/(t/h)/a
+# methanol_synthesis_cost = 42.0 * ureg.EUR / ureg.kW  # in EUR/KW/a
+methanol_synthesis_cost = 42.0 * ureg.EUR / (ureg.t / ureg.h)
 
 
 balanceCO2H2 = 7.268519  # from gams file
 # balanceCO2H2 * h2 = co2
-# --> balanceCO2H2 * h2 + h2 = co2 + h2
-# --> (balanceCO2H2 + 1) * h2 = co2 + h2
-# --> h2 / (co2 + h2) = 1/(balanceCO2H2 + 1)
 methanol_synthesis_input_proportions = {
-    "co2": 1 - 1 / (balanceCO2H2 + 1),
-    "electrolizer": 1 / (balanceCO2H2 + 1),
+    "co2": 1 * ureg.kg / ureg.h,
+    "hydrogen": balanceCO2H2 * ureg.kg / ureg.h,
 }
 
 methanolSysnthesisEff = 5.093 * 5.54 * 1e3
-methanol_synthesis_convert_factor = 1 / (balanceCO2H2 + 1) * methanolSysnthesisEff
+# XXX should be checked if this is correct
+methanol_synthesis_convert_factor = methanolSysnthesisEff
 
 storage_params = {
     "electricity": {
@@ -81,8 +94,11 @@ dollar_to_eur = 0.848
 # n = 20; i = 0.08; ((1+i)**n * i) / ((1+i)**n-1)
 co2_cost_lifetime = 694  # $/(t/year) for a certain life time
 co2_cost_opex = 26  # $/t-CO2 (this assumes 90% utilization, i.e. variable costs are included)
-co2_cost = dollar_to_eur * (
-    (capital_recovery_factor * co2_cost_lifetime + co2_cost_opex) * 8760
+co2_cost = (
+    dollar_to_eur
+    * ((capital_recovery_factor * co2_cost_lifetime + co2_cost_opex) * 8760)
+    * ureg.EUR
+    / (ureg.t / ureg.h)
 )  # EUR/(t_CO2/h)/a
 
 co2_electricity_input = 366  # kWh/t-CO2
@@ -90,7 +106,7 @@ co2_gas_input = 5.25  # GJ/t-CO2
 gj_to_kwh = 1 / 3.6e-3
 gas_efficiency = 1 / 3  # assumes heat pump COP3
 co2_convert_factor = 1 / (gj_to_kwh * co2_gas_input * gas_efficiency + co2_electricity_input)
+co2_convert_factor = co2_convert_factor * ureg.t / ureg.kWh  # XXX is the unit correct?
 
-# probably in KWh/Year, right?
 # TODO what is a reasonable value here? shouldn't the result be proportional to this choice?
-methanol_demand = 1000
+methanol_demand = np.array(1000) * ureg.t / ureg.h
