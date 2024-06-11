99839052e678b274aa751f9bdf1ec93c488a02d6
lumbric <lumbric@gmail.com>
2024-06-11 18:16:03 +0200

    Minor improvements for the task decorator

--- src/task.py
+++ src/task.py
@@ -10,9 +10,9 @@ from src.logging_config import setup_logging
 
 
 def get_git_hash():
-    """Return a GIT ref of the current repository.
-    If the d
-    See git-describe for more information."""
+    """Return a GIT ref of the current repository. This can either be a tag or a commit hash. If
+    there are modified files (uncommitted changes), '-dirty' will be appended to the GIT ref.  See
+    git-describe for more information."""
     result = subprocess.run(
         ["git", "describe", "--tags", "--always", "--dirty"],
         capture_output=True,
@@ -22,18 +22,27 @@ def get_git_hash():
 
 def task(func):
     @wraps(func)
-    def task_func(inputs, outputs, **input_params):
+    def task_func(inputs, outputs, allow_dirty_git=False, **input_params):
         """
+
         Parameters
         ----------
         func_path
         inputs : list FIXME could this be a dict too?
         outputs : list
         allow_dirty_git : bool
-        input_params : dict
+        input_params : kwargs
 
         """
-        # TODO use click?
+        # TODO would it make sense to use a Python library like click to get a CMD interface for
+        # calling a function decorated with @task?
+
+        # TODO can we check that this is a call from another function decorated with @task? It
+        # would be nice to support nested calls, but ATM this is kinda weird, it would overwrite
+        # the run yaml files and may cause other troubles.
+
+        if not allow_dirty_git:
+            ...
 
         # TODO this does not really make sense here, I guess tasks are run in the same python
         # process... How can we have a separate log file for each task?
@@ -41,36 +50,31 @@ def task(func):
 
         start_time = time.time()
 
-        # XXX can we check that this is not a call from another function decorated with @task?
-        # would be nice to support nested calles, but ATM this is kinda weird, it would overwrite
-        # the run yaml files and may cause other troubles
-
         run_data = {
             # 'run_id': ???
-            # params <--- some params are encoded indirectly via inputs
             # config
             # IDs of the inputs?
             "function": func.__name__,
+            # FIXME might be huge, any ideas how to handle it?
             "input_params": input_params,
-            # FIXME might be huge, any ideas?
             "input_files": list(inputs),
             "output_files": list(outputs),
             "start_time": datetime.now().astimezone().isoformat(),
             "runtime": float("nan"),
+            "error_msg": "",
             "hostname": socket.gethostname(),
             "git_commit": get_git_hash(),
         }
 
-        try:
-            result = func(inputs=inputs, outputs=outputs, **input_params)
-        finally:
-            run_data["runtime"] = time.time() - start_time
+        # TODO do we want to catch exceptions here and safe the error message and run time or not?
+        result = func(inputs=inputs, outputs=outputs, **input_params)
 
-            for output in outputs:
-                fname = f"{output}.run.yaml"
-                print(fname)
-                with open(fname, "w") as f:
-                    yaml.safe_dump(run_data, f)
+        run_data["runtime"] = time.time() - start_time
+
+        for output in outputs:
+            fname = f"{output}.run.yaml"
+            with open(fname, "w") as f:
+                yaml.safe_dump(run_data, f)
 
         return result
 
